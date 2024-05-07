bc6f720bfd6524fb3be4f871dfebd4a9c15fd715
Roland-Sankara <roland1sankara@gmail.com>
2024-05-07 10:22:58 +0300

    add routing with react router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="m-8">
+      <h1 className="font-sans text-amber-500 text-5xl p-4">Movie Ranking Site</h1>
+      <p className="text-2xl p-4">This is the home page</p>
+    </div>
+  );
+}
+
+export default Home;
