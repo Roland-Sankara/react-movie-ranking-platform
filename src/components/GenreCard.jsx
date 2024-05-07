7546d642e8ccc474f364b300fb0aa33558002f10
Roland-Sankara <roland1sankara@gmail.com>
2024-05-07 11:40:23 +0300

    add genre component

---
+++ src/components/GenreCard.jsx
@@ -0,0 +1,12 @@
+function GenreCard(props){
+    return(
+        <div className="w-[25%] rounded-lg bg-[#212846] text-center">
+            <div>
+                <img className="w-full rounded-t-lg" src={props.image} alt="card picture" />
+            </div>
+            <h2 className="text-2xl font-bold text-white p-4">{props.genre}</h2>
+        </div>
+    )
+}
+
+export default GenreCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
+import GenreCard from "../components/GenreCard";
 
 function Home() {
   return (
@@ -15,8 +16,25 @@ function Home() {
         image="https://movie-ranking-platform.netlify.app/images/ghostbusters-action.webp"
       />
 
-      <h1>Movie Genres</h1>
-      
+      <h1 className="font-sans text-white text-5xl text-center p-8 font-bold mb-4">Movie Genres</h1>
+      <section className="w-[70%] m-auto pb-8 flex justify-around">
+        <GenreCard 
+            genre="Action"
+            image="https://movie-ranking-platform.netlify.app/images/Kings-man-action1.webp"
+        />
+        <GenreCard 
+            genre="Adventure"
+            image="https://movie-ranking-platform.netlify.app/images/jumanji-commedy1.webp"
+        />
+        <GenreCard 
+            genre="Sci-fiction"
+            image="https://movie-ranking-platform.netlify.app/images/ant-man-1.webp"
+        />
+        <GenreCard 
+            genre="Romance"
+            image="https://movie-ranking-platform.netlify.app/images/When-in-rome-romance3.jpg"
+        />
+      </section>
     </div>
   );
 }
